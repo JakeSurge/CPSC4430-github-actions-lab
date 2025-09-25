070915b69e191537feb5a321247b8e036470e2cf
Jake Surgeon <jakobsurge@gmail.com>
2025-09-24 21:23:14 -0400

    Add test file with basic unit tests for each endpoint

---
+++ test_app.py
@@ -0,0 +1,32 @@
+import pytest
+from app import app
+
+@pytest.fixture()
+def client():
+    return app.test_client()
+
+
+def test_home(client):
+    response = client.get("/")
+
+    assert b"Best Calculator Site Ever" in response.data, "Home page is missing title"
+
+
+def test_add(client):
+    response = client.get("/add?a=1&b=2")
+
+    assert b"The sum is 3" in response.data, "Adding page is not adding correctly"
+
+
+def test_subtract(client):
+    response = client.get("/subtract?a=1&b=2")
+
+    assert b"The difference is -1" in response.data, "Subtracting page is not subtracting correctly"
+
+
+def test_multiply(client):
+    response = client.get("/multiply?a=1&b=2")
+
+    assert b"The product is 2" in response.data, "Multiplying page is not multiplying correctly"
+
+
